dc7903f7af0f853cc2c2622ea1bca515919e64bf
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2023-11-09 00:49:38 -0800

    made LightAttack state last as long as the animation length
    Also added some hang time for air attacks (currently making the player stop on the ground which feels not great, I would like the player to be able to still input movement maybe but not sure how to do that with the state machine approach)

--- Assets/Scripts/Player/StateMachine/States/LightAttackState.cs
+++ Assets/Scripts/Player/StateMachine/States/LightAttackState.cs
@@ -11,6 +11,8 @@ namespace PlayerStateMachine
         private PlayerWeapon _playerWeapon;
         private SpriteRenderer _sprite;
 
+        private float _attackTime;
+
         public LightAttackState(Player player) : base(player)
         {
             _playerWeapon = Player.transform.GetComponentInChildren<PlayerWeapon>();
@@ -21,25 +23,42 @@ namespace PlayerStateMachine
         {
             _startTime = Player.ElapsedTime;
             _playerWeapon.TurnOnHitbox();
-            _sprite.enabled = true;
+            // _sprite.enabled = true;
             List<EnemyHealth> hitEnemies = _playerWeapon.GetEnemiesInHitbox();
 
             foreach (var enemy in hitEnemies)
             {
                 enemy.Damage(Stats.LightAttackDamage);
             }
+
+            Player.Animator.SetTrigger("Attack1");
+
+            AnimationClip[] clips = Player.Animator.runtimeAnimatorController.animationClips;
+            foreach (var clip in clips)
+            {
+                if (clip.name == "Player-Attack")
+                    _attackTime = clip.length;
+            }
+
+            if (!TriggerInfo.OnGround)
+            {
+                Player.SetGravity(0);
+                Player.SetVelocity(Player.Velocity.x, 0);
+            }
+            else Player.SetVelocity(0, 0);
         }
 
         public override void UpdateState()
         {
-            if (Player.ElapsedTime > _startTime + 0.05f)
+            if (Player.ElapsedTime > _startTime + _attackTime)
                 Player.SetState(PlayerStateType.Move);
         }
 
         public override void ExitState()
         {
-            _sprite.enabled = false;
+            // _sprite.enabled = false;
             Player.SetLightAttackCooldown();
+            Player.SetGravity(GameManager.Instance.PlayerStats.RisingGravity);
         }
     }
 }
