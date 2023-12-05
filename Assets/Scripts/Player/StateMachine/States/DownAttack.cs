b010eff83f640d54a02d5477274b9d7a4d37467b
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2023-12-04 18:07:35 -0800

    create down and up attack states

---
+++ Assets/Scripts/Player/StateMachine/States/DownAttack.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace PlayerStateMachine
+{
+    public class DownAttack : PlayerState
+    {
+        public DownAttack(Player player) : base(player) { }
+    }
+}
---
+++ Assets/Scripts/Player/StateMachine/States/UpAttack.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace PlayerStateMachine
+{
+    public class UpAttack : PlayerState
+    {
+        public UpAttack(Player player) : base(player) { }
+    }
+}
