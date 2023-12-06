fe2330df1e21a856a057312bfb34cdab57b719dd
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2023-12-06 00:21:01 -0800

    refactor

--- Assets/Scripts/Environment/Hazard/Hazard.cs
+++ Assets/Scripts/Environment/Hazard/Hazard.cs
@@ -6,13 +6,19 @@ public class Hazard : MonoBehaviour
     public int HazardDamage = 1;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Health health = other.gameObject.GetComponent<PlayerHealth>();
+        PlayerHealth health = other.gameObject.GetComponent<PlayerHealth>();
         if (health != null)
         {
-            ((PlayerHealth)health).DamageAndRespawn(HazardDamage, Vector2.up); // fix knockback direction
+            health.DamageAndRespawn(HazardDamage, GetDirection()); // fix knockback direction
             GameManager.Instance.RespawnPlayer();
         }
         else
             other.gameObject.GetComponent<EnemyHealth>()?.Kill();
+    }
+
+    private Vector2 GetDirection()
+    {
+        // TODO: how to calculate the facing direction of the hazard
+        return Vector2.right;
     }
 }
