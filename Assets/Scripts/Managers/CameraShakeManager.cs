d1b336c240fb4dcefd895962ae7fdeb9f6a1cd84
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2023-12-05 17:35:34 -0800

    trying out controller rumble

--- Assets/Scripts/Managers/CameraShakeManager.cs
+++ Assets/Scripts/Managers/CameraShakeManager.cs
@@ -1,9 +1,15 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Cinemachine;
 
 public class CameraShakeManager : MonoBehaviour
 {
     public static CameraShakeManager Instance;
+
+    private Gamepad pad;
+    [Range(0f, 1f)] public float ControllerRumbleIntensity = 1f;
+    [Range(0f, 1f)] public float ControllerRumbleDuration = 0.2f;
 
     private void Awake()
     {
@@ -16,5 +22,29 @@ public class CameraShakeManager : MonoBehaviour
     public void CameraShake(CinemachineImpulseSource impulseSource, float force)
     {
         impulseSource.GenerateImpulseWithForce(force);
+        SetRumblePulse(ControllerRumbleIntensity, ControllerRumbleDuration);
+    }
+
+    private float _currentStrength = 0;
+    private float _rumbleDuration;
+    public void SetRumblePulse(float strength, float duration)
+    {
+        pad = Gamepad.current;
+        _currentStrength = strength;
+        _rumbleDuration = duration;
+        StartCoroutine(DoRumble());
+    }
+
+    private IEnumerator DoRumble()
+    {
+        float time = 0;
+        while (time < _rumbleDuration)
+        {
+            float newStrength = Mathf.Lerp(_currentStrength, 0, time / _rumbleDuration);
+            pad?.SetMotorSpeeds(newStrength, 0.7f * newStrength);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        pad?.SetMotorSpeeds(0, 0);
     }
 }
